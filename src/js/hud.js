42cff58eb89f855357431ba9fea8e8e5ba21d6d4
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2020-04-20 15:54:00 +0200

    Rewrite rendering pump with requestAnimationFrame

--- src/js/hud.js
+++ src/js/hud.js
@@ -1,13 +1,13 @@
-function step(player, _input, ctx, area, state) {
-  const {x, y, width, height} = area;
+function step(player, _input, { ctx }, area) {
+  const { x, y, width, height } = area;
 
-	ctx.clearRect(x, y, width, height);
+  ctx.clearRect(x, y, width, height);
 
-	ctx.font = '16px bold Arial, Helvetica, sans-serif';
-	ctx.textBaseline = 'top';
-	ctx.fillText(`LEVEL: ${Math.floor(state.level)}`, 10 + x, 5 + y);
-	ctx.fillText(`SCORE: ${Math.floor(player.score)}`, 160 + x, 5 + y);
-	ctx.fillText(`HEALTH: ${Math.floor(player.health)}`, 310 + x, 5 + y);
+  ctx.font = '16px bold Arial, Helvetica, sans-serif';
+  ctx.textBaseline = 'top';
+  ctx.fillText(`LEVEL: ${Math.floor(player.level)}`, 10 + x, 5 + y);
+  ctx.fillText(`SCORE: ${Math.floor(player.score)}`, 160 + x, 5 + y);
+  ctx.fillText(`HEALTH: ${Math.floor(player.health)}`, 310 + x, 5 + y);
 }
 
 export default { step };
--- src/js/level.js
+++ src/js/level.js
@@ -3,34 +3,34 @@ import { mod } from './utils.js';
 const SIZE = 50;
 const STARVATION = 0.15;
 
-let level = 0;
 let snakes = [];
 
-function step(player, input, ctx, area, state) {
-  const {x, y, width, height} = area;
+function step(player, input, { dt, ctx }, area) {
+  const { x, y, width, height } = area;
+  const timeScale = dt / 70; // This is an arbitrary number that seems to work well.
 
   // Proceed to first/next level when there are no snakes
   if (snakes.filter(Boolean).length === 0) {
-    snakes = Array(++level * 2).fill(0).map(() => ({
-      v: {x: 0, y: 0},
-      path: [{x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height)}],
+    snakes = Array(++player.level * 2).fill(0).map(() => ({
+      v: { x: 0, y: 0 },
+      path: [{ x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height) }],
       size: SIZE,
       hue: Math.floor(Math.random() * 360)
     }));
   }
-  
+
   ctx.clearRect(x, y, width, height);
   
   // Move the player
-  movePlayer(player, input, area);
-  player.score += .5;
+  movePlayer(timeScale, player, input, area);
+  player.score += timeScale * .5;
   
   for (let s = 0; s < snakes.length; s++) {
     const snake = snakes[s];
     if (!snake) continue;
 
     // Move the snake
-    moveSnake(snake, player, area);
+    moveSnake(timeScale, snake, player, area);
 
     // Eat the player, if in range
     const head = snake.path[snake.path.length - 1];
@@ -38,36 +38,37 @@ function step(player, input, ctx, area, state) {
       head.x > player.position.x &&
       head.x < (player.position.x + player.img.width / 2) &&
       head.y > player.position.y &&
-      head.y < (player.position.y + player.img.height / 2)) {
-        snake.size += SIZE;
+      head.y < (player.position.y + player.img.height / 2)
+    ) {
+      snake.size += SIZE;
+      
+      // TODO: Audio?
+      //document.getElementById('death').play();
+      
+      if (snake.size > SIZE * 2) {
+        // Split the path in two
+        const path = snake.path;
         
-        // TODO: Audio
-        //document.getElementById('death').play();
+        // Keep the head
+        snake.path = path.slice(-SIZE);
+        snake.size = SIZE;
         
-        if (snake.size > SIZE * 2) {
-          // Split the path in two
-          const path = snake.path;
-          
-          // Keep the head
-          snake.path = path.slice(-SIZE);
-          snake.size = SIZE;
-          
-          // A new snake is born.
-          snakes.push({v: {x: 0, y: 0}, path: [head], size: SIZE, hue: mod(snake.hue + Math.floor(Math.random() * 90) - 45, 360) });
-        }
-        
-        // TODO: make this suck less
-        // Place the player at a random position
-        player.position = {
-          x: Math.floor(Math.random() * (width - player.img.width / 2)),
-          y: Math.floor(Math.random() * (height - player.img.height / 2))
-        };
+        // A new snake is born.
+        snakes.push({ v: { x: 0, y: 0 }, path: [head], size: SIZE, hue: mod(snake.hue + Math.floor(Math.random() * 90) - 45, 360) });
+      }
+      
+      // TODO: make this suck less
+      // Place the player at a random position
+      player.position = {
+        x: Math.floor(Math.random() * (width - player.img.width / 2)),
+        y: Math.floor(Math.random() * (height - player.img.height / 2))
+      };
 
-        player.health -= 10;
+      player.health -= 10;
     }
     
     // Hunger games
-    snake.size -= STARVATION;
+    snake.size -= timeScale * STARVATION;
     if (snake.size <= 1) {
       // Remove the starved snake
       snakes[s] = null;
@@ -96,47 +97,48 @@ function step(player, input, ctx, area, state) {
       ctx.drawImage(player.img, player.position.x + x, player.position.y + y, player.img.width / 2, player.img.height / 2);
     }
   }
-
-  state.level = level;
 }
   
-function movePlayer(player, input, {width, height}) {
+function movePlayer(timeScale, player, input, { width, height }) {
+  const speed = timeScale * 2.5;
   if (player.position) {
     // Left - right
     if (input.keys[37]) {
-      player.position.x = Math.max(player.position.x - 3, 0);
+      player.position.x = Math.max(player.position.x - speed, 0);
     } else if (input.keys[39]) {
-      player.position.x = Math.min(player.position.x + 3, width - player.img.width / 2);
+      player.position.x = Math.min(player.position.x + speed, width - player.img.width / 2);
     }
     
     // Up - down
     if (input.keys[38]) {
-      player.position.y = Math.max(player.position.y - 3, 0);
+      player.position.y = Math.max(player.position.y - speed, 0);
     } else if (input.keys[40]) {
-      player.position.y = Math.min(player.position.y + 3, height - player.img.height / 2);
+      player.position.y = Math.min(player.position.y + speed, height - player.img.height / 2);
     }
   }
 }
 
-function moveSnake(snake, player, {width, height}) {
+function moveSnake(timeScale, snake, player, { width, height }) {
   // Add something to the vector
-  snake.v.x += Math.random() * 1 - .5;
-  snake.v.y += Math.random() * 1 - .5;
+  snake.v.x += timeScale * (Math.random() - .5);
+  snake.v.y += timeScale * (Math.random() - .5);
   
   // Max speed
-  snake.v.x = Math.min(Math.max(snake.v.x, -4), 4);
-  snake.v.y = Math.min(Math.max(snake.v.y, -4), 4);
+  const maxSpeed = timeScale * 4;
+  snake.v.x = Math.min(Math.max(snake.v.x, -maxSpeed), maxSpeed);
+  snake.v.y = Math.min(Math.max(snake.v.y, -maxSpeed), maxSpeed);
   
   // Steer towards the player
+  const steerSpeed = timeScale * .1;
   const head = snake.path[snake.path.length - 1];
   if (player.position) {
-    snake.v.x += head.x > (player.position.x + player.img.width / 4) ? -.1 : .1;
-    snake.v.y += head.y > (player.position.y  + player.img.height / 4) ? -.1 : .1;
+    snake.v.x += head.x > (player.position.x + player.img.width / 4) ? -steerSpeed : steerSpeed;
+    snake.v.y += head.y > (player.position.y  + player.img.height / 4) ? -steerSpeed : steerSpeed;
   }
   
   // add the vector to the path
   const lastPoint = snake.path[snake.path.length - 1];
-  const newPoint = {x: lastPoint.x + snake.v.x, y: lastPoint.y + snake.v.y};
+  const newPoint = { x: lastPoint.x + snake.v.x, y: lastPoint.y + snake.v.y };
   
   // bounce
   if (newPoint.x < 0 || newPoint.x > width) {
