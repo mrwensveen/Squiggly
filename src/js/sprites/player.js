033ae22bd544bb94eba961c913ea7ceeefb6ea85
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2020-11-14 23:34:43 +0100

    ESLint

--- src/js/sprites/player.js
+++ src/js/sprites/player.js
@@ -1,27 +1,41 @@
 import * as utils from '../utils.js';
-import * as powerupSprite from '../sprites/powerup.js';
+import * as powerupSprite from './powerup.js';
 
-export function draw(player, ctx, { x, y }) {
+function draw(player, ctx, { x, y }) {
   if (!player.position) return;
 
-  ctx.drawImage(player.img, player.position.x + x, player.position.y + y, player.img.width, player.img.height);
+  ctx.drawImage(
+    player.img,
+    player.position.x + x,
+    player.position.y + y,
+    player.img.width,
+    player.img.height,
+  );
 
   if (player.powerup && player.powerup.type === 'shield' && player.powerup.active) {
     const center = utils.playerCenter(player);
 
     const shieldGradient = ctx.createRadialGradient(center.x, center.y, 0, center.x, center.y, 100);
     shieldGradient.addColorStop(0, 'transparent');
-    shieldGradient.addColorStop(.9, 'rgba(0, 127, 255, .3)');
+    shieldGradient.addColorStop(0.9, 'rgba(0, 127, 255, .3)');
     shieldGradient.addColorStop(1, 'transparent');
 
     ctx.fillStyle = shieldGradient;
     ctx.fillRect(center.x - 100, center.y - 100, 200, 200);
 
     ctx.save();
-    ctx.globalAlpha = .3;
+    ctx.globalAlpha = 0.3;
 
     const puImg = powerupSprite.POWERUP_IMGS.get(player.powerup.type);
-    ctx.drawImage(puImg, center.x - player.powerup.value, center.y - player.powerup.value, player.powerup.value * 2, player.powerup.value * 2);
+    ctx.drawImage(
+      puImg,
+      center.x - player.powerup.value,
+      center.y - player.powerup.value,
+      player.powerup.value * 2,
+      player.powerup.value * 2,
+    );
     ctx.restore();
   }
 }
+
+export default draw;
